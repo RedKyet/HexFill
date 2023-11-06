a20e0485e1d488a7f38b7dcb00facfa1be0a299f
AndyGooShooter <91827827+AndyGooShooter@users.noreply.github.com>
2023-11-06 21:40:41 +0200

    synced driver with main; created text output

--- driver.py
+++ driver.py
@@ -26,13 +26,14 @@ source = [0, 0]
 
 # mat = pixel matrix
 def fill(mat, source, mode):
-    # mode = 0 --> empty (score calculation)
+    # mode = 0 --> empty (score calculation, kill bombs)
     # mode = 1 --> blanks (bleaching bombs)
     # mode = 2, 3 or 4 --> superfill (color bombs)
     
     sum = 0
     def recursion(current):
         nonlocal sum
+        nonlocal mat
         x = current[0]
         y = current[1]
         color = current[2]
@@ -74,22 +75,62 @@ def fill(mat, source, mode):
                 recursion([x + 1, y - 1, color])
             
     recursion(source)
-    return sum
+    return [sum, mat]
         
-def calculate_score(mat):
-    score = 0
-    for i in range(len(mat)):
-        for j in range(len(mat[i])):
-            if mat[i][j][2] != 0 and mat[i][j][2] != 1:
-                score += max(1, fill(mat, [i, j, mat[i][j][2]], 0)) ** 2
-    return score
+def master(mat, prev_score):
+    def calculate_score(mat):
+        score = 0
+        highest_area = 0
+        highest_color = 0
 
+        for i in range(len(mat)):
+            for j in range(len(mat[i])):
+                if mat[i][j][2] != 0 and mat[i][j][2] != 1:
+                    color = mat[i][j][2]
+                    add = fill(mat, [i, j, mat[i][j][2]], 0)[0]
+                    
+                    if add >= highest_area:
+                        highest_area = add
+                        highest_color = color
+                        
+                    score += add ** 2
+        return [score, highest_area, highest_color]
+    
+    result = calculate_score(mat)
+    score = result[0]
+    highest_area = result[1]
+    highest_color = result[2]
+    
+    score_diff = score - prev_score
+    
+    score_TEXT = str(score)
+    score_diff_TEXT = '+ ' + str(score_diff)
+    if(score_diff > 5):
+        score_diff_TEXT += ' Combo!'
+        
+    highest_TEXT = ''
+    if highest_color == 2:
+        highest_TEXT = 'Cea mai mare zonă este galbenă! (' + str(highest_area) + ' pixeli)'
+    elif highest_color == 3:
+        highest_TEXT = 'Cea mai mare zonă este mov! (' + str(highest_area) + ' pixeli)'
+    elif highest_color == 4:
+        highest_TEXT = 'Cea mai mare zonă este verde! (' + str(highest_area) + ' pixeli)'
+    # elif highest_color == 5:
+    #     highest_TEXT = '-1'
+    
+    return [score_TEXT, score_diff_TEXT, highest_TEXT]
+
+def bomb(mat, source):
+    mat = fill(mat, source, 0)[1]
+    return mat
+    
 def bleach(mat, source):
-    fill(mat, source, 1)
+    mat = fill(mat, source, 1)[1]
+    return mat
 
 def color(mat, source, mode):
-    fill(mat, source, mode)
-
+    mat = fill(mat, source, mode)[1]
+    return mat
 
 test_mat = [
 [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],  # 0 (TOP)
@@ -97,12 +138,15 @@ test_mat = [
 [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],  # 2
 [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],             # 3
 [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],  # 4
-[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],             # 5
-[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],  # 6
-[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],             # 7
-[[0, 0, 0], [0, 0, 0], [3, 3, 3], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],  # 8
-[[0, 0, 0], [0, 0, 0], [3, 3, 3], [0, 0, 0], [0, 0, 0], [0, 0, 0]],             # 9
-[[1, 1, 1], [3, 3, 3], [3, 3, 3], [3, 3, 3], [0, 0, 0], [0, 0, 0], [0, 0, 0]],  # 10
-[[1, 1, 1], [1, 1, 1], [3, 3, 3], [3, 3, 3], [0, 0, 0], [0, 0, 0]],             # 11 (BOTTOM)
+[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 4], [0, 0, 4], [0, 0, 4]],             # 5
+[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 4], [0, 0, 4], [0, 0, 4], [0, 0, 1]],  # 6
+[[0, 0, 0], [0, 0, 0], [0, 0, 4], [0, 0, 4], [0, 0, 1], [0, 0, 1]],             # 7
+[[0, 0, 0], [0, 0, 4], [0, 0, 3], [0, 0, 1], [0, 0, 1], [0, 0, 3], [0, 0, 2]],  # 8
+[[0, 0, 4], [0, 0, 4], [0, 0, 3], [0, 0, 1], [0, 0, 3], [0, 0, 2]],             # 9
+[[0, 0, 1], [0, 0, 3], [0, 0, 3], [0, 0, 3], [0, 0, 2], [0, 0, 3], [0, 0, 3]],  # 10
+[[0, 0, 1], [0, 0, 1], [0, 0, 3], [0, 0, 3], [0, 0, 3], [0, 0, 3]],             # 11 (BOTTOM)
 ]
-print(calculate_score(test_mat))
+text = master(test_mat, 0)
+print(text[0])
+print(text[1])
+print(text[2])
