45edd02a7d00ddb7e7e79c030bcad0ee08b80de1
AndyGooShooter <91827827+AndyGooShooter@users.noreply.github.com>
2023-11-06 15:12:15 +0200

    assets

--- driver.py
+++ driver.py
@@ -13,7 +13,7 @@ mat = [
 ]
 # even rows --> 0 - 5 (6)
 # odd rows --> 0 - 6 (7) 
-source = [0, 0, 0]
+source = [0, 0]
 # (x, y, color)
 
 # color = 4 --> no pixel here
@@ -24,15 +24,38 @@ source = [0, 0, 0]
 
 # mat = pixel matrix
 # N = size of matrix
-def lee_fill(mat, cols, source):
-    def findNeighbors(mat, current):
+def lee(mat, source, action):
+    q = deque()
+    def propagate(mat, current):
         x = current[0]
         y = current[1]
         color = current[2]
-        if mat[x][y + 1][2] == 
-    q = deque()
+        
+        if mat[x][y + 1][2] == color:
+            mat[x][y + 1][2] = 4
+            q.append([x, y + 1])
+        if mat[x][y - 1][2] == color:
+            mat[x][y - 1][2] = 4
+            q.append([x, y - 1])
+        if mat[x + 1][y][2] == color:
+            mat[x + 1][y][2] = 4
+            q.append([x + 1, y])
+        if mat[x - 1][y][2] == color:
+            mat[x - 1][y][2] = 4
+            q.append([x - 1, y])
+        if mat[x + 1][y + 1][2] == color:
+            mat[x + 1][y + 1][2] = 4
+            q.append([x + 1, y + 1])
+        if mat[x + 1][y - 1][2] == color:
+            mat[x + 1][y - 1][2] = 4
+            q.append([x + 1, y - 1])
+            
     q.append(source)
     while q:
         current = q.popleft()
+        propagate(mat, current)
+        
+def calculate_score():
+    
         
     
